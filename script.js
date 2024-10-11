4ac51604a74a79ed24d38ee63d8316d3599da1cc
Rafika_MK <rafikahouda2005@gmail.com>
2024-10-11 22:39:46 +0100

    nrmlmn finish

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const bookSection = document.querySelector(".bookSection");
 const submit = document.querySelector(".btn");
 const submit2 = document.querySelector(".btn2");
 const titel2 = document.querySelector(".titel2");
+const search = document.querySelector(".search");
+const search_btn = document.querySelector(".search_btn");
+const home = document.querySelector(".home");
 
 const titel = document.querySelector(".titel");
 const author = document.querySelector(".author");
@@ -17,6 +20,7 @@ const book2 = new Book("SOUL2", "OLIVIA WILSON", "500","Finished", cover);
 const book3 = new Book("SOUL3", "OLIVIA WILSON", "500", "Finished", cover);
 const book4 = new Book("SOUL4", "OLIVIA WILSON", "500", "Not Finished", cover);
 const book5 = new Book("SOUL5", "OLIVIA WILSON", "500", "Not Finished", cover);
+const book6 = new Book("SOUL6", "OLIVIA WILSON", "500", "Not Finished", cover);
 
 
 addBookToLibrary(book1);
@@ -24,7 +28,7 @@ addBookToLibrary(book2);
 addBookToLibrary(book3);
 addBookToLibrary(book4);
 addBookToLibrary(book5);
-addBookToLibrary(book4);
+addBookToLibrary(book6);
 
 displayBooks();
 
@@ -42,26 +46,87 @@ function closeForm() {
     document.getElementById("myForm2").style.display = "none";
  }
 
-submit.addEventListener("click",(event)=>{
+ home.addEventListener("click",()=>{
+    bookSection.innerHTML="";
+    displayBooks();
+ })
+ search_btn.addEventListener("click",(event)=>{
 
     event.preventDefault();
+    let i = searchBook(search.value);
 
-    const bookn = new Book (titel.value ,author.value,pages.value,read.value,cover);
+    bookSection.innerHTML = "";
 
-    addBookToLibrary(bookn);
-    displayBooks();
+    if(i!=-1){
 
+        
+        
+        displayBook(i);
+
+    }else{
+        bookNotFound();
+    }
+
+    search.reset();
+ });
+
+ submit.addEventListener("click", (event) => {
+
+    const forma = document.querySelector(".form-container");
+    event.preventDefault();  
+    if (forma.checkValidity()){
+        const bookn = new Book(titel.value, author.value, pages.value, read.checked, cover);
+
+        addBookToLibrary(bookn);
+
+        bookSection.innerHTML = "";
+        displayBooks();
+        forma.reset();
+    }else{
+
+        forma.reportValidity();
+
+    }
+         
+        
 });
 
 submit2.addEventListener("click",(event)=>{
 
-    event.preventDefault();
+    
 
-    removeBookFromLibrary(titel2.value);
+    const forma = document.getElementById("forma")
+    event.preventDefault();  
 
-    displayBooks();
+    if (forma.checkValidity()){
+       
+        removeBookFromLibrary(titel2.value);
+    
+        bookSection.innerHTML="";
+    
+        displayBooks();
+        forma.reset();
 
+    }else{
+
+        forma.reportValidity();
+    }
+
+       
+        
 });
+
+
+function searchBook(titel){
+
+    for(let i=0 ; i<myLibrary.length ; i++){
+        if( titel === myLibrary[i].titel){
+            return i;
+        }
+    }
+    return -1;
+   
+}
 
 function Book(titel, author, pages, read, bookCover) {
 
@@ -79,20 +144,11 @@ function addBookToLibrary(book) {
 
 }
 function removeBookFromLibrary(titel){
-    console.log("titel");
+        let k = searchBook(titel);
+            myLibrary.splice( k ,1);
+
+            console.log("heree");   
     
-    for(let i=0 ; i< myLibrary.length ; i++){
- 
-        console.log(i);
-        if (titel === myLibrary[i].titel){
-
-            myLibrary.splice(i,1);
-
-            console.log("heree");
-            break;
-
-        }
-    }
 }
 
 function displayBooks() {
@@ -100,8 +156,15 @@ function displayBooks() {
 
     for (let i = 0; i < myLibrary.length; i++) {
 
+        displayBook(i);
+       
+    }
 
-        const imag = document.createElement('img');
+}
+
+
+function displayBook(i ){
+     const imag = document.createElement('img');
         const name = document.createElement('h2');
         const author = document.createElement('h4');
         const page = document.createElement('h4');
@@ -134,11 +197,15 @@ function displayBooks() {
 
 
         bookSection.appendChild(element);
-    }
-
 }
 
+function bookNotFound(){
 
+    const notFound = document.createElement('h2');
+    const element = document.createElement("div");
 
+    notFound.textContent="No Such Book With This Name .";
+    element.appendChild(notFound);
+    bookSection.appendChild(element);
 
-
+}
